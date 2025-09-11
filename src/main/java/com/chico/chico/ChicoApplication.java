d33a79dadcec2a91608a9d2e26df61741210ad3b
wojte <wojciech@kosow.pl>
2025-09-11 19:38:23 +0200

    initial commit

---
+++ src/main/java/com/chico/chico/ChicoApplication.java
@@ -0,0 +1,13 @@
+package com.chico.chico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chico/chico/ChicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chico.chico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
